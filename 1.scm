0f0b8b5c8206c0a173397fa8067aeeb970aea12e
hanachin <hanachin@gmail.com>
2011-08-11 20:04:50 +0900

    p5 two-in-a-row? is-first?
    p5で定義されてるis-first?とtwo-in-a-row?を定義
    is-first?はaとlatの最初の要素が同じかどうかを判定する補助関数
    two-in-a-row?はアトムが連続して2回現れるかどうかを調べる

--- 1.scm
+++ 1.scm
@@ -9,3 +9,17 @@
 (define sub1
   (lambda (x)
     (- x 1)))
+
+;; p5
+;; aとlatの最初の要素が同じかどうかを判定する補助関数
+(define is-first?
+  (lambda (a lat)
+    (cond ((null? lat) #f)
+          (else (eq? (car lat) a)))))
+
+;; アトムが連続して2回現れるかどうかを調べる
+(define two-in-a-row?
+  (lambda (lat)
+    (cond ((null? lat) #f)
+          (else (or (is-first? (car lat) (cdr lat))
+                    (two-in-a-row? (cdr lat)))))))
