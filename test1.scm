6c2637513163fd44cef54f17993ec19947360fdf
hanachin <hanachin@gmail.com>
2011-08-11 19:53:08 +0900

    Chapter 1のテストケースを作成
    Chapter 1に出て来る質問に対応したGauche用のテストケースを作成した。 1章に対応した、1.scmを読み込んでいる。
    two-in-a-row?のテストケース4つを書いてある。

---
+++ test1.scm
@@ -0,0 +1,22 @@
+(use gauche.test)
+(add-load-path ".")
+(load "1")
+
+(test-start "chapter 1")
+
+(test-section "two-in-a-row?")
+;; p3
+(test* "(two-in-a-row? 'sardines '(Italian sardines spaghetti parsley))"
+       #f
+       (two-in-a-row? 'sardines'(Italian sardines spaghetti parsley)))
+;; p4
+(test* "(two-in-a-row? 'sardines '(Italian sardines sardines spaghetti parsley))"
+       #t
+       (two-in-a-row? 'sardines '(Italian sardines sardines spaghetti parsley)))
+(test* "(two-in-a-row? 'sardines '(Italian sardines more sardines spaghetti parsley))"
+       #f
+       (two-in-a-row? 'sardines '(Italian sardines more sardines spaghetti parsley)))
+;; p9
+(test* "(two-in-a-row? '(b d e i i a g))" #t (two-in-a-row? '(b d e i i a g)))
+
+(test-end)
