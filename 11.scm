c951e1e800111fb99776e79f58bdf8455f40dd59
hanachin <hanachin@gmail.com>
2011-08-28 07:53:04 +0900

    p33 change two-in-a-row?

--- 11.scm
+++ 11.scm
@@ -14,19 +14,16 @@
   (lambda (x)
     (= x 1)))
 
-;; p7
-(define two-in-a-row-b?
-  (lambda (preceding lat)
-    (cond ((null? lat) #f)
-          (else (or (eq? (car lat) preceding)
-                    (two-in-a-row-b? (car lat) (cdr lat)))))))
-
-;; p7
-;; アトムが連続して2回現れるかどうかを調べる(改訂版)
+;; p33 letrec version
 (define two-in-a-row?
   (lambda (lat)
-    (cond ((null? lat) #f)
-          (else (two-in-a-row-b? (car lat) (cdr lat))))))
+    (letrec
+        ((W (lambda (a lat)
+              (cond ((null? lat) #f)
+                    (else (or (eq? (car lat) a)
+                              (W (car lat) (cdr lat))))))))
+      (cond ((null? lat) #f)
+            (else (W (car lat) (cdr lat)))))))
 
 ;; p10
 (define sum-of-prefixes-b
