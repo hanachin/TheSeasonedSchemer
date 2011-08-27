8fa2a0c637ee4788a1c8537e50ad89cbbf2fbdb3
hanachin <hanachin@gmail.com>
2011-08-28 06:48:30 +0900

    p27 added union

--- 12.scm
+++ 12.scm
@@ -24,3 +24,11 @@
                        ((eq? a (car l)) #t)
                        (else (yes? (cdr l)))))))
          (yes? lat))))
+
+;; p27
+(define union
+  (lambda (set1 set2)
+    (cond ((null? set1) set2)
+          ((member? (car set1) set2) (union (cdr set1) set2))
+          (else (cons (car set1)
+                      (union (cdr set1) set2))))))
