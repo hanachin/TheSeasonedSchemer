240aa98bc1b6cf4e1d2f8a56c31915f30f57e173
hanachin <hanachin@gmail.com>
2011-08-28 06:22:44 +0900

    p22 change multirember

--- 12.scm
+++ 12.scm
@@ -4,14 +4,13 @@
     ((lambda (f) (f f))
      (lambda (f) (le (lambda (x) ((f f) x)))))))
 
-;; p18 letrec version
+;; p22 letrec version
 (define multirember
   (lambda (a lat)
-    ((letrec
-         ((mr (lambda (lat)
-                (cond ((null? lat) '())
-                      ((eq? a (car lat)) (mr (cdr lat)))
-                      (else (cons (car lat)
-                                  (mr (cdr lat))))))))
-       mr)
-     lat)))
+    (letrec
+        ((mr (lambda (lat)
+               (cond ((null? lat) '())
+                     ((eq? a (car lat)) (mr (cdr lat)))
+                     (else (cons (car lat)
+                                 (mr (cdr lat))))))))
+      (mr lat))))
