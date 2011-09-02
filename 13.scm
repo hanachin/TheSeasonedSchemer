fc7a76aa4a03e5b12e0ac3072029264b3f2ecc92
hanachin <hanachin@gmail.com>
2011-09-02 22:55:54 +0900

    p39 change intersectall

--- 13.scm
+++ 13.scm
@@ -22,7 +22,10 @@
 ;; p38
 (define intersectall
   (lambda (lset)
-    (cond ((null? lset) '())
-          ((null? (cdr lset)) (car lset))
-          (else (intersect (car lset)
-                           (intersectall (cdr lset)))))))
+    (letrec
+        ((A (lambda (lset)
+              (cond ((null? (cdr lset)) (car lset))
+                    (else (intersect (car lset)
+                                     (A (cdr lset))))))))
+      (cond ((null? lset) '())
+            (else (A lset))))))
