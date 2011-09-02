21499e314cddffcfef4b930072355c5edd521000
hanachin <hanachin@gmail.com>
2011-09-02 23:12:38 +0900

    p41 change intersectall
    intersectall letcc version

--- 13.scm
+++ 13.scm
@@ -19,13 +19,15 @@
                     (else (I (cdr set)))))))
       (I set1))))
 
-;; p38
+;; p41
 (define intersectall
   (lambda (lset)
-    (letrec
-        ((A (lambda (lset)
-              (cond ((null? (cdr lset)) (car lset))
-                    (else (intersect (car lset)
-                                     (A (cdr lset))))))))
-      (cond ((null? lset) '())
-            (else (A lset))))))
+    (let/cc hop
+      (letrec
+          ((A (lambda (lset)
+                (cond ((null? (car lset)) (hop '()))
+                      ((null? (cdr lset)) (car lset))
+                      (else (intersect (car lset)
+                                       (A (cdr lset))))))))
+        (cond ((null? lset) '())
+              (else (A lset)))))))
