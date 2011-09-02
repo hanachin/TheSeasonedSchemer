77bf09b4ca2e2ef09d62f2122d2737ef6d7140d4
hanachin <hanachin@gmail.com>
2011-09-02 23:05:39 +0900

    p39, p40 added tests for chapter 13

--- test13.scm
+++ test13.scm
@@ -9,4 +9,17 @@
        '(and macaroni)
        (intersect '(tomatoes and macaroni) '(macaroni and cheese)))
 
+(test-section "intersectall")
+;; p39
+(test* "(intersectall '((3 mangos and) (3 kiwis and) (3 hamburgers)))"
+       '(3)
+       (intersectall '((3 mangos and) (3 kiwis and) (3 hamburgers))))
+;; p40
+(test* "(intersectall '((3 steaks and) (no food and) (three baked potatoes) (3 diet hamburgers)))"
+       '()
+       (intersectall '((3 steaks and) (no food and) (three baked potatoes) (3 diet hamburgers))))
+(test* "(intersectall '((3 mangoes and) () (3 diet hamburgers)))"
+       '()
+       (intersectall '((3 mangoes and) () (3 diet hamburgers))))
+
 (test-end)
