22d0f7451bf09a6d46f412385c9d69185ea23280
hanachin <hanachin@gmail.com>
2011-09-06 07:52:00 +0900

    fix leftmost

--- 14.scm
+++ 14.scm
@@ -5,7 +5,7 @@
 ;; p65
 (define leftmost
   (lambda (l)
-    (cond ((null? (car l)) '())
+    (cond ((null? l) '())
           ((atom? (car l)) (car l))
           (else (cond ((atom? (leftmost (car l))) (leftmost (car l)))
                       (else (leftmost (cdr l))))))))
