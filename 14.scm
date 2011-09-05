241f5ee934e8769dde63a598d05669f0a65bccd7
hanachin <hanachin@gmail.com>
2011-09-06 07:35:11 +0900

    p65 change leftmost

--- 14.scm
+++ 14.scm
@@ -2,8 +2,10 @@
   (lambda (x)
     (and (not (pair? x)) (not (null? x)))))
 
-;; p63
+;; p65
 (define leftmost
   (lambda (l)
-    (cond ((atom? (car l)) (car l))
-          (else (leftmost (car l))))))
+    (cond ((null? (car l)) '())
+          ((atom? (car l)) (car l))
+          (else (cond ((atom? (leftmost (car l))) (leftmost (car l)))
+                      (else (leftmost (cdr l))))))))
