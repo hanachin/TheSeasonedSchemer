f2d1e79fa8a909c0b47a71e2ce80cb344be03af7
hanachin <hanachin@gmail.com>
2011-09-06 08:15:57 +0900

    p67 added rember*

--- 14.scm
+++ 14.scm
@@ -11,3 +11,21 @@
            (let ((a (leftmost (car l))))
              (cond ((atom? a) a)
                    (else (leftmost (cdr l)))))))))
+
+(define eqlist? equal?)
+
+;; p67
+(define rember*
+  (lambda (a l)
+    (cond ((null? l) '())
+          ((atom? (car l))
+           (cond ((eq? (car l) a) (cdr l))
+                 (else (cons (car l)
+                             (rember* a (cdr l))))))
+          (else
+           (cond ((eqlist? (rember* a (car l))
+                           (car l))
+                  (cons (car l)
+                        (rember* a (cdr l))))
+                 (else (cons (rember* a (car l))
+                             (cdr l))))))))
