0e843ee336dd75ab08681d792f56d3250e7c5c02
hanachin <hanachin@gmail.com>
2011-09-24 23:20:19 +0900

    p112 added deep

--- 16.scm
+++ 16.scm
@@ -2,3 +2,12 @@
 (define sweet-tooth
   (lambda (food)
     (cons food (cons 'cake '()))))
+
+(define sub1 (lambda (n) (- n 1)))
+
+;; p112
+(define deep
+  (lambda (m)
+    (cond ((zero? m) 'pizza)
+          (else (cons (deep (sub1 m))
+                      '())))))
