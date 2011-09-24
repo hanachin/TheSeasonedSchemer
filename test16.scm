4c6ef38f2f0ff3d6390ea858c72466f7eeb53614
hanachin <hanachin@gmail.com>
2011-09-24 23:15:17 +0900

    added deep tests

--- test16.scm
+++ test16.scm
@@ -8,4 +8,10 @@
 (test* "(sweet-tooth 'chocolate)" '(chocolate cake) (sweet-tooth 'chocolate))
 (test* "(sweet-tooth 'fruit)" '(fruit cake) (sweet-tooth 'fruit))
 
+(test-section "deep")
+;; p112
+(test* "(deep 3)" '(((pizza))) (deep 3))
+(test* "(deep 7)" '(((((((pizza))))))) (deep 7))
+(test* "(deep 0)" 'pizza (deep 0))
+
 (test-end)
